3af1342161c21471e2ce9a1e095c0962d4b950a0
JeCFe <38367384+JeCFe@users.noreply.github.com>
2022-02-02 12:33:21 +0000

    Added improvements

--- main.py
+++ main.py
@@ -7,12 +7,16 @@ import calendar
 import random
 import wikipedia
 import playsound
+import pyttsx3
 from pydub import AudioSegment
 from pydub.playback import play
+from pyttsx3 import voice
 
 warnings.filterwarnings('ignore')
-
-
+#Set up the text to speech voice
+voiceEngine = pyttsx3.init()
+voices = voiceEngine.getProperty('voices')
+voiceEngine.setProperty('voice', voices[0].id)
 
 def listenToAudio():
     r = sr.Recognizer()
@@ -21,7 +25,6 @@ def listenToAudio():
         audio = r.listen(source)
     data = ''
     try:
-
         data = r.recognize_google(audio)
         print("You said " + data)
     except sr.UnknownValueError:
@@ -33,21 +36,31 @@ def listenToAudio():
 
 
 
-def miniResponse(data):
+def speechResponse(data):
+    print("here")
     print(data)
-    myobject = gTTS(text = data, lang ='en', slow=False)
-    myobject.save('assistant_response.mp3')
-    playsound.playsound('assistant_response.mp3', True)
+    voiceEngine.say(data)
+    voiceEngine.runAndWait()
 
 
-
+#this will listen for wake word then will return what was said after the wake word
 def wakeCommand(data):
-    wake_phrases = ['ok mini', 'hey mini']
+    wake_phrases = ['omega','hey omega']
     data = data.lower()
+    command = ""
+    iterator = 0
+
     for item in wake_phrases:
         if item in data:
-            return True
-    return False
+            offset = len(item)
+            for i in range(iterator + offset, len(data)):
+                command = command + data[i]
+
+            return command.lstrip(' ')
+        iterator +=1
+    command = ""
+    return command
+
 
 def getDate():
     now = datetime.datetime.now()
@@ -82,11 +95,42 @@ def greeting(data):
     return ""
 
 
+def pullName(data):
+    wordArray = data.split()
+    name = ""
+    for i in range(0, len(wordArray)):
+        if i + 3 <= len(wordArray):
+            if (wordArray[i].lower() == 'who' and wordArray[i+1].lower() == 'is'):
+                for j in range(i+2, len(wordArray)):
+                    name = name + wordArray[j] + " "
+            elif(wordArray[i] == "who's"):
+                for j in range(i+1, len(wordArray)):
+                    name = name + wordArray[j] + " "
+        return name
+
+
+def manageCommandOption(data):
+    command = data.split()
+
+    if (command[0] == 'who' and command[1] == 'is') or command[0] == "who's":
+        wikiResponse = ""
+        name = pullName(data).title()
+        name = ''.join(name.split())
+        print(name)
+        wikiInfo = wikipedia.summary(name, sentences = 2)
+        speechResponse(wikiInfo)
+
+
+
+while (True):
+    data = listenToAudio()
+    response = ''
+    command = wakeCommand(data)
+    if(command != "" ): #We know there has been a command given
+        manageCommandOption(command)
 
 
 
 
-print(getDate())
-miniResponse(getDate())
 
 
