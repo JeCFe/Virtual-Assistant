83127cf8f02124d632c5e818b204065033598471
JeCFe <38367384+JeCFe@users.noreply.github.com>
2022-02-02 14:27:37 +0000

    Update main.py
    Added a config file system that currently allows for personal names

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 import speech_recognition as sr
 import os
-from gtts import gTTS
+
 import datetime
 import warnings
 import calendar
 import random
 import wikipedia
-import playsound
+import json
 import pyttsx3
-from pydub import AudioSegment
-from pydub.playback import play
+
 from pyttsx3 import voice
 
 warnings.filterwarnings('ignore')
 #Set up the text to speech voice
 voiceEngine = pyttsx3.init()
 voices = voiceEngine.getProperty('voices')
-voiceEngine.setProperty('voice', voices[0].id)
+voiceEngine.setProperty('voice', voices[2].id)
 
 def listenToAudio():
     r = sr.Recognizer()
@@ -34,7 +33,9 @@ def listenToAudio():
     return data
 
 
-
+def whatQuestion(data):
+    response = ""
+    return response
 
 def speechResponse(data):
     print("here")
@@ -49,7 +50,6 @@ def wakeCommand(data):
     data = data.lower()
     command = ""
     iterator = 0
-
     for item in wake_phrases:
         if item in data:
             offset = len(item)
@@ -69,13 +69,11 @@ def getDate():
     month = now.month
     day = now.day
     year = now.year
-
     monthData = ['January', 'February', 'March', 'April', 'may', 'June', 'July',
                  'August', 'September', 'October', 'November', 'December']
-
     amendedNumber = ""
     day = str(day)
-    if(day == '1' or day == '21' or day == '31'):
+    if day == '1' or day == '21' or day == '31':
         amendedNumber = day + "st"
     elif(day == '2' or day == '22'):
         amendedNumber = day + "nd"
@@ -83,8 +81,8 @@ def getDate():
         amendedNumber = day + "rd"
     else:
         amendedNumber = day + "th"
-
     return "Today is  " + str(weekday)+" "+ str(amendedNumber) + " of " + str(monthData[month - 1] )+ " " + str(year)
+
 
 def greeting(data):
     input = ['hello', 'hi']
@@ -95,39 +93,77 @@ def greeting(data):
     return ""
 
 
+
 def pullName(data):
     wordArray = data.split()
     name = ""
     for i in range(0, len(wordArray)):
         if i + 3 <= len(wordArray):
-            if (wordArray[i].lower() == 'who' and wordArray[i+1].lower() == 'is'):
+            if wordArray[i].lower() == 'who' and wordArray[i + 1].lower() == 'is':
                 for j in range(i+2, len(wordArray)):
                     name = name + wordArray[j] + " "
-            elif(wordArray[i] == "who's"):
+            elif wordArray[i] == "who's":
                 for j in range(i+1, len(wordArray)):
                     name = name + wordArray[j] + " "
         return name
 
 
+
+
 def manageCommandOption(data):
     command = data.split()
-
+    speakResponse = ""
     if (command[0] == 'who' and command[1] == 'is') or command[0] == "who's":
-        wikiResponse = ""
         name = pullName(data).title()
         name = ''.join(name.split())
-        print(name)
-        wikiInfo = wikipedia.summary(name, sentences = 2)
-        speechResponse(wikiInfo)
+        speakResponse = wikipedia.summary(name, sentences = 2)
+
+    elif command[0] == "what":
+        speakResponse = whatQuestion(data)
+
+    else: #If the command wasnt understood
+        speakResponse = "Sorry I didnt quite understand that"
+    speechResponse(speakResponse)
+
+
+class PersonalInformaion:
+    name = ""
+    def __init__(self): #Sets up the configuration files
+        try:
+            with open('config_data.json', 'r') as jsonFile:
+                print("here")
+                data = json.load(jsonFile)
+                name = data['name']
+        except:
+            print("here")
+            speechResponse("What is your name")
+            name = listenToAudio()
+            data = {'name' : name}
+            with open('config_data.json', 'w') as jsonFile:
+                json.dump(data, jsonFile)
+            #Double check right name was recieved
+
+
+        response = "I am Omega, welcome " + name
+        speechResponse(response)
+
+def main():
+    personInfo = PersonalInformaion()
+    while (True):
+        data = listenToAudio()
+        response = ''
+        command = wakeCommand(data)
+        if (command != ""):  # We know there has been a command given
+            manageCommandOption(command)
 
 
 
-while (True):
-    data = listenToAudio()
-    response = ''
-    command = wakeCommand(data)
-    if(command != "" ): #We know there has been a command given
-        manageCommandOption(command)
+if __name__ == "__main__":
+    main()
+
+
+
+
 
 
 
